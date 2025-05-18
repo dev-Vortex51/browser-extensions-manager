a41faf02b8c1405fca032ec055286ba34fc2ad0d
Bello Qudus Folohunsho <qudusbello51@gmail.com>
2025-05-18 19:02:10 +0100

    Refactor Container component and update index.html for improved structure and clarity

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -4,7 +4,7 @@ import Wrapper from "./Wrapper";
 
 function Container() {
   return (
-    <div className="mx-auto w-[90%] md:w-[90%] lg:w-[80%] h-full grid grid-rows-[auto_auto_1fr] gap-10">
+    <div className="mx-auto w-[95%] md:w-[90%] lg:w-[80%] h-full grid grid-rows-[auto_auto_1fr] gap-10">
       <Header />
       <SmallHeader />
       <Wrapper />
